4c0dac720c1437ef48262d4dad000a9f5cdb5e93
Richie Leonardo <richie.leonardoo@gmail.com>
2025-05-22 13:34:27 +0700

    Make reservation-detail-page, change style, update, root

--- src/scripts/pages/profile/profile-page.js
+++ src/scripts/pages/profile/profile-page.js
@@ -1,11 +1,28 @@
 export default class ProfilePage {
     async render(){
         return `
-        <h1>Profile Page</h1>
+        <div class="container valign-wrapper">
+          <h1 id="profile-title"">Your Profile</h1>
+          <div class="profile-container"></div>
+          </div>
+          <div class="button-container">
+            <a>Reservation List</a>
+            <a>Log Out</a>
+          </div>
+        </div>
         `
     }
 
     async afterRender(){
+        this._fillProfileContainer();
+    }
 
+    _fillProfileContainer(user){
+        return `
+          <div id=user-name class=card-panel>${user.name}</div>
+          <div id=user-email class=card-panel>${user.email}</div>
+          <div id=user-password class=card-panel>${user.password}</div>
+          
+        `;
     }
 }
--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
 import LoginPage from "../pages/login/login";
 import ProfilePage from '../pages/profile/profile-page';
+import ReservationPage from '../pages/reservation-detail/reservation-detail-page';
 
 const routes = {
   '/': new HomePage(),
   '/about': new AboutPage(),
   '/login': new LoginPage(),
   '/profile': new ProfilePage(),
+  '/reservation/:id': new ReservationPage(),
 };
 
 export default routes;
