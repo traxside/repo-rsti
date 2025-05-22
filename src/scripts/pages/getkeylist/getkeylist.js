f68ffd4906a3313c47f195808f333ab43c9f3545
Richie Leonardo <richie.leonardoo@gmail.com>
2025-05-22 18:39:21 +0700

    style reservation pages and getkeylist

--- src/scripts/pages/getkeylist/getkeylist.js
+++ src/scripts/pages/getkeylist/getkeylist.js
@@ -1,38 +1,42 @@
 export default class GetKeyListPage {
   async render() {
     return `
-        <div class="container row valign-wrapper">
+        <div class="container row center-align header-reservation">
             <a class="col back" href="#/reservationslist"><img class="icon-detail" src="images/back_arrow_purple.png" alt="back button"></a>
-            <div class="reserve title two">Kuningan Apartment</div>
+            <h1 class="reserve title two">Kuningan Apartment</h1>
         </div>
-
-        <div class="container">
-            <div class="profile title">Check Out</div>
-        </div>
-
-        <div class="container">
-            <div class="profile title">House Rules</div>
-            <!-- Tambah padding -->
-            <div class="container rules-element">
-                <ol>
-                <li>Check-In Code is used to lock/unlock the property</li>
-                <li>Check-In will be available at 12 PM</li>
-                <li>Check-Out will be available at 11 AM</li>
-                <li>Please note that all furniture and accessories are part of the property and are not complimentary. Kindly do not remove or take any items from the premises</li>
-                </ol>
-            </div>
-        </div>
-
-        <!-- Button -->
-        <div class="container profile">
-            <!-- Check Out Button dibuat agar data di database hilang -->
-            <a class="waves-effect waves-light btn">Check Out</a>
-            <a class="waves-effect waves-light btn merah">Chat - Not Available</a>
-        </div>
+        <section class="container">
+  
+          <div class="profile title">Check Out</div>
+          
+          <div class="card-panel z-depth-0 center-align key-container">000000</div>
+          
+          <div class="profile title">House Rules</div>
+          <!-- Tambah padding -->
+          <div class="container rules-element">
+              <ol>
+              <li>Check-In Code is used to lock/unlock the property</li>
+              <li>Check-In will be available at 12 PM</li>
+              <li>Check-Out will be available at 11 AM</li>
+              <li>Please note that all furniture and accessories are part of the property and are not complimentary. Kindly do not remove or take any items from the premises</li>
+              </ol>
+          </div>
+  
+          <!-- Button -->
+          <div class="container profile">
+              <!-- Check Out Button dibuat agar data di database hilang -->
+              <a class="waves-effect waves-light btn">Check Out</a>
+              <a class="waves-effect waves-light btn merah">Chat - Not Available</a>
+          </div>
+        
+        </section>
     `;
   }
 
   async afterRender() {
 
   }
+
+
+
 }
--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,12 +3,13 @@ import AboutPage from '../pages/about/about-page';
 import LoginPage from "../pages/login/login";
 import ProfilePage from '../pages/profile/profile-page';
 import SignUpPage from '../pages/signup/signup';
-import ReservationsListPage from '../pages/reservationslist/reservationslist';
-import ReservationDetailDetailPage from "../pages/reservation-detail/reservation-detail-page";
+import ReservationsListPage from '../pages/reservations-list/reservations-list-page';
+import ReservationDetailPage from "../pages/reservation-detail/reservation-detail-page";
 import GetKeyListPage from '../pages/getkeylist/getkeylist';
 
 const routes = {
   '/': new HomePage(),
+  '/home': new HomePage(),
   '/about': new AboutPage(),
   '/login': new LoginPage(),
   '/profile': new ProfilePage(),
