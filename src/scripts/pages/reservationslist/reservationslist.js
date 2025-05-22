1501de0e0df46953bfdaf9757c1491ce8c899fdb
BryantGouw <116624580+Bryantgouw@users.noreply.github.com>
2025-05-22 14:45:53 +0700

    Membuat get key list page, memperbaiki reservation list

--- src/scripts/pages/reservationslist/reservationslist.js
+++ src/scripts/pages/reservationslist/reservationslist.js
@@ -8,7 +8,7 @@ export default class ReservationsListPage {
 
         <div class="container">
             <div class="card-panel property">
-                <a class="property-detail class" href=#reservation>
+                <a class="property-detail class" href="#/getkeylist">
                     <div class="property-title">Kuningan Apartment</div>
                     <div class="property-desc">21/03/2025 - 23/03/2025</div>
                 </a>
--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -4,7 +4,8 @@ import LoginPage from "../pages/login/login";
 import ProfilePage from '../pages/profile/profile-page';
 import SignUpPage from '../pages/signup/signup';
 import ReservationsListPage from '../pages/reservationslist/reservationslist';
-import ReservationPage from '../pages/reservation-detail/reservation-detail-page';
+import ReservationDetailPage from '../pages/reservation-detail/reservation-detail-page';
+import GetKeyListPage from '../pages/getkeylist/getkeylist';
 
 const routes = {
   '/': new HomePage(),
@@ -13,7 +14,8 @@ const routes = {
   '/profile': new ProfilePage(),
   '/signup': new SignUpPage(),
   '/reservationslist': new ReservationsListPage(),
-  '/reservation/:id': new ReservationPage(),
+  '/reservation/:id': new ReservationDetailPage(),
+  '/getkeylist': new GetKeyListPage(),
 };
 
 export default routes;
