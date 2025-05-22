79f05b3e01c136c09a5e823af84fffff91061478
Richie Leonardo <richie.leonardoo@gmail.com>
2025-05-22 14:30:44 +0700

    change footer visibility logic, and complete reservation-detail-page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -4,13 +4,17 @@ import LoginPage from "../pages/login/login";
 import ProfilePage from '../pages/profile/profile-page';
 import SignUpPage from '../pages/signup/signup';
 import ReservationsListPage from '../pages/reservationslist/reservationslist';
-import ReservationPage from '../pages/reservation-detail/reservation-detail-page';
+import ReservationDetailPage from "../pages/reservation-detail/reservation-detail-page";
 
 const routes = {
   '/': new HomePage(),
   '/about': new AboutPage(),
   '/login': new LoginPage(),
   '/profile': new ProfilePage(),
+  '/signup': new SignUpPage(),
+  '/reservationslist': new ReservationsListPage(),
+  '/reservation/:id': new ReservationDetailPage(),
+
 };
 
 export default routes;
